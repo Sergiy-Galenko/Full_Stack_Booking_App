b26fab225098c35d7cfc740a68a7f29d535fef00
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-05-13 12:42:04 +0300

    Update

--- Full_Stack_Booking_App/screens/HomeScreen.js
+++ Full_Stack_Booking_App/screens/HomeScreen.js
@@ -1,53 +1,138 @@
-import { Text, View, StyleSheet, ScrollView, Pressable } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Pressable,
+  TextInput,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import Header from "../componens/Header";
+import DatePicker from "react-native-date-ranges";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: true,
-      title:"Booking.com",
-      headerTitleStyle:{
-        fontSize:20,
-        fontWeight:"bold",
-        color:"white"
+      title: "Booking.com",
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
       },
-      headerStyle:{
-        backgroundColor:"#003580",
-        height:110,
-        borderBottomColor:"transparent",
-        shadowColor:"transparent",
+      headerStyle: {
+        backgroundColor: "#003580",
+        height: 110,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
       },
-      headerRight : () =>(
-        <Ionicons name="notifications-outline" size={24} color="white" style={{marginRight:12}}/>
-      )
+      headerRight: () => (
+        <Ionicons
+          name="notifications-outline"
+          size={24}
+          color="white"
+          style={{ marginRight: 12 }}
+        />
+      ),
     });
   }, []);
+  
 
   return (
     <View>
-      <Header/>
-      <ScrollView style={{margin:20,borderColor:"#FFC72C",borderWidth:3,borderRadius:6}}>
+      <Header />
+      <ScrollView
+        style={{
+          margin: 20,
+          borderColor: "#FFC72C",
+          borderWidth: 3,
+          borderRadius: 6,
+        }}
+      >
         <View>
-            {/*Destation // Место назначения*/}
-            <Pressable>
-
-            </Pressable>
-            {/*Selected Dates// Выбранные даты*/}
-            <Pressable>
-
-            </Pressable>
-            {/*Rooms and Guests// Номера и гости*/}
-            <Pressable>
-
-            </Pressable>
-            {/*Search Button// Номера и гости*/}
-            <Pressable>
-
-            </Pressable>
+          {/*Destination // Место назначения*/}
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              paddingHorizontal: 10,
+              borderColor: "#FFC72C",
+              borderWidth: 2,
+              paddingVertical: 15,
+            }}
+          >
+            <Feather name="search" size={24} color="black" />
+            <TextInput placeholder="Enter your Destination" />
+          </Pressable>
+          {/*Selected Dates// Выбранные даты*/}
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              paddingHorizontal: 10,
+              borderColor: "#FFC72C",
+              borderWidth: 2,
+              paddingVertical: 15,
+            }}
+          >
+            <Feather name="calendar" size={24} color="black" />
+            <DatePicker
+              style={{ width: 350, height: 30, borderRadius:0,borderWidth:0, borderColor:"transper"}}
+              customStyles={{
+                placeholderText: {
+                  fontSize: 15,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginRight: "auto",
+                },
+                headerStyle: {
+                  backgroundColor: "#003580",
+                },
+                contentText: {
+                  fontSize: 15,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginRight: "auto",
+                },
+              }} 
+              selectedBgColor="#0047AB"
+              customButtom={(onConfirm) =>  customButtom(onConfirm) }
+              onConfirm={}
+              allowFontScaling={false}
+              placeholder={"Apr 27, 2018 → Jul 10, 2018"}
+              mode={"range"}
+            />
+          </Pressable>
+          {/*Rooms and Guests// Номера и гости*/}
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              paddingHorizontal: 10,
+              borderColor: "#FFC72C",
+              borderWidth: 2,
+              paddingVertical: 15,
+            }}
+          ></Pressable>
+          {/*Search Button// Кнопка поиска*/}
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              paddingHorizontal: 10,
+              borderColor: "#FFC72C",
+              borderWidth: 2,
+              paddingVertical: 15,
+            }}
+          ></Pressable>
         </View>
       </ScrollView>
     </View>
